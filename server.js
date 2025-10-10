b3429258946f7c65b24daadf03b56cab6edb7b0c
itoledo1 <itoledo1@outlook.com>
2025-10-10 16:04:59 -0500

    creo que ahora si sirve

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
